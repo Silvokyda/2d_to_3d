371c8a6824acb1f4bffc69d352abddb69d157dfd
Silvanus Oketch <deseve48@gmail.com>
2023-11-25 08:43:33 +0300

    3d

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi import FastAPI, File, UploadFile, WebSocket, Depends
+from fastapi.responses import StreamingResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import HTTPException
 import os
 import torch
 from PIL import Image
@@ -10,81 +12,93 @@ from shap_e.util.notebooks import create_pan_cameras, decode_latent_images, gif_
 from io import BytesIO
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-# Function to load models and diffusion configuration
+
+# WebSocket for real-time updates
+class WebSocketUpdate(WebSocket):
+    async def on_connect(self, websocket):
+        await websocket.accept()
+
+
 def load_models(models_path):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    xm = load_model('transmitter', device=device)
+    # Load the transmitter model
+    xm = load_model('transmitter')
     xm.load_state_dict(torch.load(os.path.join(models_path, 'xm_model.pth')))
     xm.eval()
 
-    text300M = load_model('text300M', device=device)
+    # Load the text300M model
+    text300M = load_model('text300M')
     text300M.load_state_dict(torch.load(os.path.join(models_path, 'text300M_model.pth')))
     text300M.eval()
 
+    # Load diffusion configuration
     diffusion_params = torch.load(os.path.join(models_path, 'diffusion_model.pth'))
     diffusion = GaussianDiffusion(**diffusion_params)
 
     return xm, text300M, diffusion
 
 
-# Define a list variable to store generated GIFs
-generated_gifs = []
+async def generate_3d_model(img_path, xm, text300M, diffusion, cameras, render_mode, size, websocket):
+    img = Image.open(img_path)
+
+    latents = sample_latents(
+        batch_size=4,
+        model=text300M,
+        diffusion=diffusion,
+        guidance_scale=15.0,
+        model_kwargs=dict(images=[img] * 4),
+        progress=True,
+        clip_denoised=True,
+        use_fp16=True,
+        use_karras=True,
+        karras_steps=64,
+        sigma_min=1e-3,
+        sigma_max=160,
+        s_churn=0,
+    )
+
+    images = []
+    for i, latent in enumerate(latents):
+        frame_images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
+        images.extend(frame_images)
+
+        # Send progress to WebSocket
+        await websocket.send_text(f'{{"type": "progress", "progress": {i / 4 * 100}}}')
+
+    # Convert to GIF
+    gif_bytes = gif_widget(images)
+
+    # Send generated image path to WebSocket
+    await websocket.send_text(f'{{"type": "image", "image": "/get_generated_image"}}')
+
+    return gif_bytes
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocketUpdate):
+    while True:
+        await websocket.receive_text()
 
 @app.post("/generate_3d_model")
-def generate_3d_model_from_image(img: UploadFile = File(...), models_path: str = 'shap_e/models'):
+async def generate_3d_model_from_image(
+    img: UploadFile = File(...),
+    models_path: str = '/content/saved',
+    websocket: WebSocketUpdate = Depends()
+):
     try:
-        os.makedirs(models_path, exist_ok=True)
-
-        img_path = os.path.join(models_path, img.filename)
+        img_path = "/tmp/" + img.filename
         with open(img_path, "wb") as buffer:
             buffer.write(img.file.read())
 
         xm, text300M, diffusion = load_models(models_path)
-        img = Image.open(img_path)
-
-        latents = sample_latents(
-            batch_size=4,
-            model=text300M,
-            diffusion=diffusion,
-            guidance_scale=15.0,
-            model_kwargs=dict(images=[img] * 4),
-            progress=True,
-            clip_denoised=True,
-            use_fp16=True,
-            use_karras=True,
-            karras_steps=64,
-            sigma_min=1e-3,
-            sigma_max=160,
-            s_churn=0,
-        )
-
+        cameras = create_pan_cameras(128, torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
         render_mode = 'nerf'
         size = 128
 
-        cameras = create_pan_cameras(size, torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
-
-        images = []
-        for i, latent in enumerate(latents):
-            frame_images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
-            images.extend(frame_images)
-
-        gif_bytes = gif_widget(images)
-
-        # Store the generated GIF in the list
-        generated_gifs.append(gif_bytes)
+        gif_bytes = await generate_3d_model(img_path, xm, text300M, diffusion, cameras, render_mode, size, websocket)
 
         return StreamingResponse(BytesIO(gif_bytes), media_type="image/gif")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-@app.get("/get_3d_images")
-def get_3d_images():
-    return JSONResponse(content={"images": generated_gifs})
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
